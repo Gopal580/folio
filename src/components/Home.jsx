209c7c9899916e60d50d684f8312db51918badd7
Gopal Patil <patilgopal5550@gmail.com>
2025-07-30 03:06:22 +0530

    sachins project last commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -39,15 +39,18 @@ const Home = () => {
           </div>
 
           {/* Button */}
-          <motion.Link
+          <motion.div
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.5 }}
+        >
+          <Link
             to="/projects"
             className="inline-block mt-8 px-6 py-3 bg-gradient-to-r from-pink-500 to-yellow-500 text-white font-semibold rounded-full shadow-lg hover:shadow-xl transition duration-300"
-            initial={{ opacity: 0, y: 10 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: 0.5 }}
           >
             View My Work
-          </motion.Link>
+          </Link>
+        </motion.div>
         </motion.div>
       </div>
     </>
